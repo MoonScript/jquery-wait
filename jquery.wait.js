097a03d44f9c9f180d8ec83739d0d21991d98913
Jason Moon <jmoon@socialcast.com>
2013-03-06 14:24:12 -0600

    Add optional context parameter
    To set as the context in the resolved callback function

--- jquery.wait.js
+++ jquery.wait.js
@@ -1,7 +1,9 @@
 // Wrap setTimeout in a Deferred
-$.wait = function(delay){
+$.wait = function(delay, context){
   var deferred = $.Deferred();
-  var timer = setTimeout(deferred.resolve, delay);
+  var timer = setTimeout(function(){
+    deferred.resolveWith(context || deferred);
+  }, delay);
   deferred.fail(function(){
     clearTimeout(timer);
   });
